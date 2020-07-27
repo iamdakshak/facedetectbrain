ebb90bfbe945525b34e812a52e0d36d6647af859
Dakshak Nagrale <dakshak1997@rediffmail.com>
2020-07-27 14:20:24 +0530

    Updates

---
+++ precache-manifest.a9119c0298572ad57e4d90f271096214.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0deb739ddb1e0337775cdb1bb619d87f",
+    "url": "/facedetectbrain/index.html"
+  },
+  {
+    "revision": "3282056c7520817daa96",
+    "url": "/facedetectbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3ffbe17575677864d894",
+    "url": "/facedetectbrain/static/css/main.a394ebf3.chunk.css"
+  },
+  {
+    "revision": "3282056c7520817daa96",
+    "url": "/facedetectbrain/static/js/2.e68ab483.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetectbrain/static/js/2.e68ab483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffbe17575677864d894",
+    "url": "/facedetectbrain/static/js/main.27606f67.chunk.js"
+  },
+  {
+    "revision": "a9b7c923a2ec6824c2ae",
+    "url": "/facedetectbrain/static/js/runtime-main.9e604065.js"
+  },
+  {
+    "revision": "0d4f318ed4283e688a203741cbb1dc77",
+    "url": "/facedetectbrain/static/media/logo.0d4f318e.png"
+  }
+]);
